205ab6603a72ae3b8b8716536e57679087fe4dd6
Kevin CO <kevinco@umontreal.ca>
2025-01-14 12:30:51 -0500

    .

--- cocofest/examples/dynamics/cycling/cycling_torque_driven.py
+++ cocofest/examples/dynamics/cycling/cycling_torque_driven.py
@@ -10,6 +10,8 @@ from bioptim import (
     BiorbdModel,
     BoundsList,
     CostType,
+    ConstraintList,
+    ConstraintFcn,
     DynamicsFcn,
     DynamicsList,
     InitialGuessList,
@@ -46,20 +48,24 @@ def prepare_ocp(
     x_center = objective["cycling"]["x_center"]
     y_center = objective["cycling"]["y_center"]
     radius = objective["cycling"]["radius"]
-    circle_coord_list = np.array(
-        [get_circle_coord(theta, x_center, y_center, radius)[:-1] for theta in np.linspace(0, -2 * np.pi, n_shooting)]
-    ).T
+    # circle_coord_list = np.array(
+    #     [get_circle_coord(theta, x_center, y_center, radius)[:-1] for theta in np.linspace(0, -2 * np.pi, n_shooting)]
+    # ).T
+
+    # Objective functions
     objective_functions = ObjectiveList()
-    objective_functions.add(
-        ObjectiveFcn.Mayer.TRACK_MARKERS,
-        weight=100,
-        axes=[Axis.X, Axis.Y],
-        marker_index=0,
-        target=circle_coord_list,
-        node=Node.ALL_SHOOTING,
-        phase=0,
-        quadratic=True,
-    )
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", phase=0, node=Node.ALL_SHOOTING)
+
+    # objective_functions.add(
+    #     ObjectiveFcn.Mayer.TRACK_MARKERS,
+    #     weight=100,
+    #     axes=[Axis.X, Axis.Y],
+    #     marker_index=0,
+    #     target=circle_coord_list,
+    #     node=Node.ALL_SHOOTING,
+    #     phase=0,
+    #     quadratic=True,
+    # )
 
     # Dynamics
     dynamics = DynamicsList()
@@ -78,20 +84,52 @@ def prepare_ocp(
 
     # Define control path constraint
     u_bounds = BoundsList()
-    u_bounds.add(key="tau", min_bound=np.array([-50, -50]), max_bound=np.array([50, 50]), phase=0)
+    u_bounds.add(key="tau", min_bound=np.array([-50, -50, 0]), max_bound=np.array([50, 50, 0]), phase=0)
 
     # Initial q guess
     x_init = InitialGuessList()
     u_init = InitialGuessList()
-    # If warm start, the initial guess is the result of the inverse kinematics and dynamics
-    if initial_guess_warm_start:
-        q_guess, qdot_guess, qddotguess = inverse_kinematics_cycling(
-            biorbd_model_path, n_shooting, x_center, y_center, radius, ik_method="trf"
-        )
-        x_init.add("q", q_guess, interpolation=InterpolationType.EACH_FRAME)
-        x_init.add("qdot", qdot_guess, interpolation=InterpolationType.EACH_FRAME)
-        u_guess = inverse_dynamics_cycling(biorbd_model_path, q_guess, qdot_guess, qddotguess)
-        u_init.add("tau", u_guess, interpolation=InterpolationType.EACH_FRAME)
+
+    # The initial guess is the result of the inverse kinematics and dynamics
+    biorbd_model_path = "../../msk_models/simplified_UL_Seth_pedal_aligned_test_one_marker.bioMod"
+    q_guess, qdot_guess, qddotguess = inverse_kinematics_cycling(
+        biorbd_model_path, n_shooting, x_center, y_center, radius, ik_method="trf"
+    )
+    x_init.add("q", q_guess, interpolation=InterpolationType.EACH_FRAME)
+    x_init.add("qdot", qdot_guess, interpolation=InterpolationType.EACH_FRAME)
+    u_guess = inverse_dynamics_cycling(biorbd_model_path, q_guess, qdot_guess, qddotguess)
+    u_init.add("tau", u_guess, interpolation=InterpolationType.EACH_FRAME)
+
+    # Constraints
+    constraints = ConstraintList()
+    cardinal_node_list = [i * int(n_shooting / 4) for i in range(4 + 1)]
+    for i in cardinal_node_list:
+        min_bound = x_init["q"].init[2][i]-x_init["q"].init[2][i]*0.01
+        max_bound = x_init["q"].init[2][i]+x_init["q"].init[2][i]*0.01
+        if min_bound > max_bound:
+            max_bound, min_bound = min_bound, max_bound
+        objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_STATE,
+                        key="q",
+                        index=2,
+                        target=x_init["q"].init[2][i],
+                        # min_bound=[min_bound],
+                        # max_bound=[max_bound],
+                        node=i,
+                        weight=100000,
+                        )
+
+    constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS,
+                    node=Node.START,
+                    first_marker="wheel_center",
+                    second_marker="global_wheel_center",
+                    axes=[Axis.X, Axis.Y],)
+
+    constraints.add(
+        ConstraintFcn.TRACK_MARKERS_VELOCITY,
+        node=Node.ALL_SHOOTING,
+        marker_index=bio_model.marker_index("wheel_center"),
+        axes=[Axis.X, Axis.Y],
+    )
 
     return OptimalControlProgram(
         [bio_model],
@@ -103,6 +141,7 @@ def prepare_ocp(
         x_init=x_init,
         u_init=u_init,
         objective_functions=objective_functions,
+        constraints=constraints,
         ode_solver=OdeSolver.RK4(),
         n_threads=8,
     )
@@ -111,7 +150,7 @@ def prepare_ocp(
 def main():
     # --- Prepare the ocp --- #
     ocp = prepare_ocp(
-        biorbd_model_path="../../msk_models/simplified_UL_Seth.bioMod",
+        biorbd_model_path="../../msk_models/simplified_UL_Seth_pedal_aligned.bioMod",
         n_shooting=100,
         final_time=1,
         objective={"cycling": {"x_center": 0.35, "y_center": 0, "radius": 0.1}},
