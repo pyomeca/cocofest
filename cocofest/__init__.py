5fe98e8449ad210db4b27b16f00f02b937405472
Kevin CO <kevinco@umontreal.ca>
2025-02-28 17:16:19 -0500

    init

--- cocofest/__init__.py
+++ cocofest/__init__.py
@@ -11,10 +11,9 @@ from .models.dynamical_model import FesMskModel
 from .models.model_maker import ModelMaker
 from .optimization.fes_ocp import OcpFes
 from .optimization.fes_id_ocp import OcpFesId
-from .optimization.fes_ocp_dynamics import OcpFesMsk
+from .optimization.fes_ocp_multibody import OcpFesMsk
 from .optimization.fes_nmpc import FesNmpc
-from .optimization.fes_ocp_nmpc_cyclic import NmpcFes
-from .optimization.fes_ocp_dynamics_nmpc_cyclic import NmpcFesMsk
+from .optimization.fes_nmpc_multibody import FesNmpcMsk
 from .integration.ivp_fes import IvpFes
 from .fourier_approx import FourierSeries
 
